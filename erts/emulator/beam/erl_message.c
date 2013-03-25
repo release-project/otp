64eee14e02f69a4f37e1fad2ba223e32758818d4
k4t3r1n4 <k4t3r1n4@gmail.com>
2013-03-25 16:43:09 +0200

    Made DTrace probe message_queued correspond to Erlang trace message receive

--- erts/emulator/beam/erl_message.c
+++ erts/emulator/beam/erl_message.c
@@ -362,9 +362,9 @@ erts_queue_dist_message(Process *rcvr,
                     tok_lastcnt = signed_val(SEQ_TRACE_T_LASTCNT(token));
                     tok_serial = signed_val(SEQ_TRACE_T_SERIAL(token));
                 }
-                DTRACE6(message_queued,
+                DTRACE7(message_queued,
                         receiver_name, size_object(msg), rcvr->msg.len,
-                        tok_label, tok_lastcnt, tok_serial);
+                        tok_label, tok_lastcnt, tok_serial, dtrace_ts());
             }
 #endif
 	    erts_queue_message(rcvr, rcvr_locks, mbuf, msg, token
@@ -403,8 +403,8 @@ erts_queue_dist_message(Process *rcvr,
              * TODO: We don't know the real size of the external message here.
              *       -1 will appear to a D script as 4294967295.
              */
-            DTRACE6(message_queued, receiver_name, -1, rcvr->msg.len + 1,
-                    tok_label, tok_lastcnt, tok_serial);
+            DTRACE7(message_queued, receiver_name, -1, rcvr->msg.len + 1,
+                    tok_label, tok_lastcnt, tok_serial, dtrace_ts());
         }
 #endif
 	mp->data.dist_ext = dist_ext;
@@ -528,9 +528,9 @@ queue_message(Process *c_p,
             tok_lastcnt = signed_val(SEQ_TRACE_T_LASTCNT(seq_trace_token));
             tok_serial = signed_val(SEQ_TRACE_T_SERIAL(seq_trace_token));
         }
-        DTRACE6(message_queued,
+        DTRACE7(message_queued,
                 receiver_name, size_object(message), receiver->msg.len,
-                tok_label, tok_lastcnt, tok_serial);
+                tok_label, tok_lastcnt, tok_serial, dtrace_ts());
     }
 #endif
 
@@ -1022,6 +1022,16 @@ erts_send_message(Process* sender,
 
 	    res = receiver->msg.len;
 
+#ifdef USE_VM_PROBES
+            if (DTRACE_ENABLED(message_queued)) {
+               DTRACE_CHARBUF(receiver_name, DTRACE_TERM_BUF_SIZE);
+               dtrace_proc_str(receiver, receiver_name);
+               DTRACE7(message_queued,
+                       receiver_name, size_object(message), receiver->msg.len,
+                       tok_label, tok_lastcnt, tok_serial, dtrace_ts());
+    }
+#endif
+
 	    if (IS_TRACED_FL(receiver, F_TRACE_RECEIVE)) {
 		trace_receive(receiver, message);
 	    }
@@ -1090,6 +1100,14 @@ erts_send_message(Process* sender,
 	LINK_MESSAGE(receiver, mp);
 	res = receiver->msg.len;
 	erts_proc_notify_new_message(receiver);
+
+#ifdef USE_VM_PROBES
+        if (DTRACE_ENABLED(message_queued)) {
+            DTRACE7(message_queued,
+                    receiver_name, (uint32_t)msize, receiver->msg.len,
+                    tok_label, tok_lastcnt, tok_serial, dtrace_ts());
+        }
+#endif
 
 	if (IS_TRACED_FL(receiver, F_TRACE_RECEIVE)) {
 	    trace_receive(receiver, message);
