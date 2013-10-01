8f9d67674f327cfadc426473eb4978e54a0d287a
k4t3r1n4 <k4t3r1n4@gmail.com>
2013-10-02 02:24:12 +0300

    Made DTrace probes scheduler_active and scheduler_inactive correspond to Erlang profile messages sent with scheduler set

--- erts/emulator/beam/erl_process.c
+++ erts/emulator/beam/erl_process.c
@@ -1936,6 +1936,11 @@ sched_waiting_sys(Uint no, ErtsRunQueue *rq)
     rq->woken = 0;
     if (erts_system_profile_flags.scheduler)
 	profile_scheduler(make_small(no), am_inactive);
+#ifdef USE_VM_PROBES
+    if(DTRACE_ENABLED(scheduler_inactive)) {
+        DTRACE2(scheduler_inactive, no, dtrace_ts());
+    }
+#endif
 }
 
 static ERTS_INLINE void
@@ -1947,6 +1952,11 @@ sched_active_sys(Uint no, ErtsRunQueue *rq)
     rq->waiting--;
     if (erts_system_profile_flags.scheduler)
 	profile_scheduler(make_small(no), am_active);
+#ifdef USE_VM_PROBES
+    if(DTRACE_ENABLED(scheduler_active)) {
+        DTRACE2(scheduler_active, no, dtrace_ts());
+    }
+#endif
 }
 
 Uint
@@ -2015,6 +2025,11 @@ sched_waiting(Uint no, ErtsRunQueue *rq)
     rq->woken = 0;
     if (erts_system_profile_flags.scheduler)
 	profile_scheduler(make_small(no), am_inactive);
+#ifdef USE_VM_PROBES
+    if(DTRACE_ENABLED(scheduler_inactive)) {
+        DTRACE2(scheduler_inactive, no, dtrace_ts());
+    }
+#endif
 }
 
 static ERTS_INLINE void
@@ -2027,6 +2042,11 @@ sched_active(Uint no, ErtsRunQueue *rq)
 	rq->waiting--;
     if (erts_system_profile_flags.scheduler)
 	profile_scheduler(make_small(no), am_active);
+#ifdef USE_VM_PROBES
+    if(DTRACE_ENABLED(scheduler_active)) {
+        DTRACE2(scheduler_active, no, dtrace_ts());
+    }
+#endif
 }
 
 static int ERTS_INLINE
@@ -5262,6 +5282,11 @@ suspend_scheduler(ErtsSchedulerData *esdp)
 
     if (erts_system_profile_flags.scheduler)
     	profile_scheduler(make_small(esdp->no), am_inactive);
+#ifdef USE_VM_PROBES
+    if(DTRACE_ENABLED(scheduler_inactive)) {
+        DTRACE2(scheduler_inactive, esdp->no, dtrace_ts());
+    }
+#endif
 
     sched_wall_time_change(esdp, 0);
 
@@ -5401,6 +5426,12 @@ suspend_scheduler(ErtsSchedulerData *esdp)
 
     if (erts_system_profile_flags.scheduler)
     	profile_scheduler(make_small(esdp->no), am_active);
+
+#ifdef USE_VM_PROBES
+    if(DTRACE_ENABLED(scheduler_active)) {
+        DTRACE2(scheduler_active, esdp->no, dtrace_ts());
+    }
+#endif
 
     if (!thr_prgr_active) {
 	erts_thr_progress_active(esdp, thr_prgr_active = 1);
